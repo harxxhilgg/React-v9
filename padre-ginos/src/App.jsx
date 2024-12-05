6503d3035b1a6f3a9e187ee052a0aef387d1034d
Harshil Patel <harshilp046@gmail.com>
2024-12-05 21:02:26 +0530

    Upto web tools.

--- padre-ginos/src/App.jsx
+++ padre-ginos/src/App.jsx
@@ -1,26 +1,15 @@
-import { createRoot } from "react-dom";
-import Pizza from "./Pizza";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Order from "./Order";
 
 const App = () => {
   return (
-    <div>
-      <h1>Padre Gino's - Order now</h1>
-      <Pizza
-        name="Pepperoni"
-        description="pep, cheese, n stuff"
-        image={"/public/pizzas/pepperoni.webp"}
-      />
-      <Pizza
-        name="Hawaiian"
-        description="Pineapple, n so much goin on here"
-        image={"/public/pizzas/hawaiian.webp"}
-      />
-      <Pizza
-        name="Americano"
-        description="American pizza"
-        image={"/public/pizzas/big_meat.webp"}
-      />
-    </div>
+    <StrictMode>
+      <div>
+        <h1>Padre Gino's - Order now</h1>
+        <Order />
+      </div>
+    </StrictMode>
   );
 };
 
--- padre-ginos/src/Pizza.jsx
+++ padre-ginos/src/Pizza.jsx
@@ -1,6 +1,13 @@
+let counter = 0;
+
 const Pizza = (props) => {
   return (
-    <div className="pizza">
+    <div
+      className="pizza"
+      onClick={() => {
+        console.log("hi");
+      }}
+    >
       <h1>{props.name}</h1>
       <p>{props.description}</p>
       <img src={props.image} alt={props.name} />
