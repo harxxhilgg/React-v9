7e2cc43d0c0b5f7be2a5fe1df07ec2830ec40f38
Harshil Patel <harshilp046@gmail.com>
2024-12-05 22:31:30 +0530

    Added logo for Title and added Pizza of the day.

--- padre-ginos/src/App.jsx
+++ padre-ginos/src/App.jsx
@@ -1,13 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Order from "./Order";
+import PizzaOfTheDay from "./PizzaOfTheDay";
 
 const App = () => {
   return (
     <StrictMode>
       <div>
-        <h1>Padre Gino's - Order now</h1>
+        <h1 className="logo">Padre Gino's - Order now</h1>
         <Order />
+        <PizzaOfTheDay />
       </div>
     </StrictMode>
   );
