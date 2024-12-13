9f1921da6dc541986a5ec110a10e90873938b614
Harshil Patel <harshilp046@gmail.com>
2024-12-14 00:09:29 +0530

    feat: added default image, when there's no image via API, somewhat refactoring

--- padre-ginos/src/Pizza.jsx
+++ padre-ginos/src/Pizza.jsx
@@ -1,16 +1,12 @@
-let counter = 0;
-
 const Pizza = (props) => {
   return (
-    <div
-      className="pizza"
-      onClick={() => {
-        console.log("hi");
-      }}
-    >
+    <div className="pizza">
       <h1>{props.name}</h1>
       <p>{props.description}</p>
-      <img src={props.image} alt={props.name} />
+      <img
+        src={props.image ? props.image : "https://picsum.photos/200"}
+        alt={props.name}
+      />
     </div>
   );
 };
